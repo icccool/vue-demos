267b2cbaab849f1385621e9244283327671c36c7
wangjibo <wangjibo@hydee.com>
2021-12-05 22:35:03 +0800

    1.集成ace-admin 2.关闭eslintrc校验规则no-undef

--- src/App.vue
+++ src/App.vue
@@ -60,25 +60,8 @@
                       </fieldset>
                     </form>
 
-
-
                   </div><!-- /.widget-main -->
 
-<!--                  <div class="toolbar clearfix">-->
-<!--                    <div>-->
-<!--                      <a href="#" data-target="#forgot-box" class="forgot-password-link">-->
-<!--                        <i class="ace-icon fa fa-arrow-left"></i>-->
-<!--                        I forgot my password-->
-<!--                      </a>-->
-<!--                    </div>-->
-
-<!--                    <div>-->
-<!--                      <a href="#" data-target="#signup-box" class="user-signup-link">-->
-<!--                        I want to register-->
-<!--                        <i class="ace-icon fa fa-arrow-right"></i>-->
-<!--                      </a>-->
-<!--                    </div>-->
-<!--                  </div>-->
                 </div><!-- /.widget-body -->
               </div><!-- /.login-box -->
 
